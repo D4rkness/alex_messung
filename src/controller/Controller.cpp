1b1147d4b5ebf33946b576d60a87afcd64da52fe
oli <oliver.skalic@uni-ulm.de>
2019-05-28 13:28:01 +0200

    Added the controller

---
+++ src/controller/Controller.cpp
@@ -0,0 +1,10 @@
+//
+// Created by main on 5/28/19.
+//
+
+#include "Controller.h"
+
+Controller::Controller(DataModel &dataModel1) {
+    this->dataModel = &dataModel1;
+}
+
---
+++ src/controller/Controller.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <src/model/DataModel.h>
+
+class Controller {
+
+private:
+    DataModel *dataModel;
+    Controller(DataModel &dataModel1);
+
+
+};
